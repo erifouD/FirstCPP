27094d182c269652515f88e2038dfed2fdfa8514
erifouD <artmandr@yandex.ru>
2022-01-22 16:12:44 +0300

    Добавьте файлы проекта.

---
+++ FirstP.cpp
@@ -0,0 +1,30 @@
+﻿// FirstP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+void function()
+{
+    float x = 12;
+    cout << x * x;
+}
+int main()
+{
+    cout << "Hello World!\n";
+    int f; //если равно 5 то o равно 145
+    cin >> f; //Ввод значения
+    int s = 13;
+    int o;
+    o = 140 + f;
+    // branch
+    if (o == 145)
+    {
+        cout << "145";
+    }
+    else
+    {
+        cout << "another";
+    }
+}
+
+
