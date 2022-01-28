47aec51e215cd63ca33345c57e3cc5412c8661a1
erifouD <artmandr@yandex.ru>
2022-01-28 19:04:25 +0300

    string operations

--- FirstP.cpp
+++ FirstP.cpp
@@ -6,10 +6,15 @@
 using namespace std;
 int main()
 {
+	string fts("Hello from installation 04");
+	string zero("Hello from installation 00");
+	cout << fts << endl << zero;
+}
+/*void previous() {
 	int x, y;
 	cout << "First number: ";  cin >> x;
 	cout << "Second number: "; cin >> y;
 	cout << "Sum quad equal: " << degree(x, y);
-}
+}*/
 
 
